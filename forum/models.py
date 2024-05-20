aac7de04d1d23c3ee9036973c2e8ea64891da3a6
Liene Briede <lienebriede84@gmail.com>
2024-05-20 09:07:15 +0000

    Add pagination and fixtures

--- forum/models.py
+++ forum/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     """
     Model for posts
     """
-
+    title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="posts"
         )
@@ -17,10 +17,9 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    updated_on = models.DateTimeField(auto_now=True)
-
+        
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"{self.author} on {self.created_on}"
+        return f"{self.title} by {self.author}"
