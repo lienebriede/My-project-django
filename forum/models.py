3f9e5f43fcdea00800ee30b6d83dd596e6a9aa2b
Liene Briede <lienebriede84@gmail.com>
2024-05-20 15:13:30 +0000

    Add function based view for post_detail

--- forum/models.py
+++ forum/models.py
@@ -18,11 +18,14 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
         
+    # ordered by newest posts first    
     class Meta:
         ordering = ["-created_on"]
 
+    # returns only two first lines of comment text
     def get_content_preview(self):
         return '\n'.join(self.content.split('\n')[:2])
 
+    # converts a string for a title in database
     def __str__(self):
         return f"{self.title} by {self.author}"
--- forum/views.py
+++ forum/views.py
@@ -12,3 +12,16 @@ class PostList(generic.ListView):
     template_name = "forum/index.html"
     paginate_by = 5
 
+
+def post_detail(request, slug):
+    """
+    Display an individual post
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "forum/post_detail.html",
+        {"post": post},
+    )
